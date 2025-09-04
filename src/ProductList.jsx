029502274cce53a2f267a2e38e4b48def8e87eb6
pakmatthew <pakfrontend0226@gmail.com>
2025-09-04 04:40:10 -0400

    feat: cart item functions

--- src/ProductList.jsx
+++ src/ProductList.jsx
@@ -9,7 +9,7 @@ function ProductList({ onHomeClick }) {
     const [showPlants, setShowPlants] = useState(false); // State to control the visibility of the About Us page
     const [addedToCart, setAddedToCart] = useState({});
 
-    const dispatch = useDispatch()
+    const dispatch = useDispatch();
 
     const plantsArray = [
         {
